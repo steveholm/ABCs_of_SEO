a1447f3c69c1d70496cd63e89fa829439da1e761
Steve Holm <steveholmphoto@me.com>
2012-11-04 16:01:54 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ABCsOfSeo::Application.config.secret_token = '3bdf72da05ca00cd91c83517a29a8e5e26930ca5a836b0a80559b5282681a323a40ab0800922d05eece1a0f613e076d03678a174bafb0bf4fa0d832c61c87a34'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+ABCsOfSeo::Application.routes.draw do
+  get "/guide" => 'guide#index', :as => :guide
+
+  
+end
